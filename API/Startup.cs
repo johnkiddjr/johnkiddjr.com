7c5a0874b59441b77bf50e8be51868d1c7d49089
John Kidd Jr <johnjr@kiddclan.com>
2022-07-05 23:35:02 -0400

    modified startup and program to allow use of kestral settings

---
+++ API/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("MainSite");
+            var mariaDbVersion = ServerVersion.AutoDetect(connectionString);
+            services.AddDbContext<MainSiteContext>(options => options.UseMySql(connectionString, mariaDbVersion));
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "api/{controller}/{action}/{id?}");
+            });
+        }
+    }
+}
