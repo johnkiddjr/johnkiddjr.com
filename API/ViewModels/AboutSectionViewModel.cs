402fee29f0de9c9ab3d28315de0ac1ecdbd0f59b
John Kidd Jr <johnjr@kiddclan.com>
2022-07-06 00:13:51 -0400

    Added view models

---
+++ API/ViewModels/AboutSectionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API.ViewModels
+{
+    public class AboutSectionViewModel
+    {
+        public string? Name { get; set; }
+        public List<AboutDetailViewModel>? Details { get; set; }
+    }
+}
