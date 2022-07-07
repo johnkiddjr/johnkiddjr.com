f01ed205775563f1f4aa5418ab88e855f1cb6128
John Kidd Jr <johnjr@kiddclan.com>
2022-07-06 21:20:39 -0400

    contact controller done

---
+++ API/ViewModels/ContactDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace API.ViewModels
+{
+    public class ContactDetailsViewModel
+    {
+        public string? Name { get; set; }
+        public string? HeaderText { get; set; }
+        public string? EmailAddress { get; set; }
+    }
+}
---
+++ API/ViewModels/FileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API.ViewModels
+{
+    public class FileViewModel
+    {
+        public string? FileName { get; set; }
+        public byte[]? FileData { get; set; }
+    }
+}
