1239b31d8818df4243fd64282f211d75d937305d
John Kidd Jr <johnjr@kiddclan.com>
2023-02-26 00:49:10 -0500

    Added controller for high scores, and migrations

---
+++ Infrastructure/Migrations/MainSite/20230226054210_AddHighScores.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations.MainSite
+{
+    /// <inheritdoc />
+    public partial class AddHighScores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageFileName",
+                table: "CardBackgrounds",
+                newName: "ImageFilename");
+
+            migrationBuilder.CreateTable(
+                name: "HighScores",
+                columns: table => new
+                {
+                    HighScoreId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Score = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HighScores", x => x.HighScoreId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Overviews",
+                keyColumn: "OverviewId",
+                keyValue: new Guid("416c2518-4151-4241-8ede-70d97ef8855c"),
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 26, 0, 42, 10, 386, DateTimeKind.Local).AddTicks(1839));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HighScores");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageFilename",
+                table: "CardBackgrounds",
+                newName: "ImageFileName");
+
+            migrationBuilder.UpdateData(
+                table: "Overviews",
+                keyColumn: "OverviewId",
+                keyValue: new Guid("416c2518-4151-4241-8ede-70d97ef8855c"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 12, 5, 14, 36, 1, 854, DateTimeKind.Local).AddTicks(6006));
+        }
+    }
+}
---
+++ Infrastructure/Models/HighScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructure.Models
+{
+    public class HighScore
+    {
+        public Guid HighScoreId { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public DateTime SubmissionDate {get;set;}
+    }
+}
